fdbffe44aa7a9894bdbcb835bc2ab00f22f5f3b0
mmgamboa <martingamboa2@gmail.com>
2021-07-13 17:09:22 -0300

    Small changes and clean file

--- gata/logmod.py
+++ gata/logmod.py
@@ -2,9 +2,22 @@ import logging
 
 __all__ = ['set_log']
 
-def set_log(name):
+def set_log(name, setLevel = 10):
+	"""
+	Arguments
+	========= 
+		name: 
+			Recommended value = __name__ . Using such value you'll know where the programm was running.
+		setLevel: int. 
+			Values according logging.setLevel = 0 (not set) - 10 (debug)- 20 (info)- 
+												30 (warning)- 40 (error)- 50 (critical)
+	Return
+	=========
+		logger instance
+	"""
+
 	logger = logging.getLogger(name)
-	logger.setLevel(logging.DEBUG)
+	logger.setLevel(setLevel)
 
 	formatmain = logging.Formatter('%(asctime)s:%(name)s:%(message)s') 
 
@@ -12,10 +25,6 @@ def set_log(name):
 	file_log.setLevel(logging.INFO)
 	file_log.setFormatter(formatmain)
 
-	#error_log = logging.FileHandler('log.txt')
-	#error_log.setLevel(logging.WARNING)
-	#error_log.setFormatter(formatmain)
-
 	logger.addHandler(file_log)
-	#logger.addHandler(error_log)
+
 	return logging.getLogger(name)
